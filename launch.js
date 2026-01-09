6911147df93f4d1f83c3cd1fb5c0a9eccc324ebc
shwtdev <gm.shwt1@gmail.com>
2026-01-10 00:28:47 +0100

    Update launch.js

--- launch.js
+++ launch.js
@@ -1,4 +1,5 @@
 const { spawn } = require("child_process");
 
+console.log("New meta")
 //spawn("node", ["game-client/app.js"], { stdio: "inherit" });
 spawn("node", ["game-server/compiled/server.js"], { stdio: "inherit" });
